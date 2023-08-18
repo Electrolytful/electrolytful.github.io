72be707a0c57486277df2ec406fcd6478a4dcbe5
Electrolytful <erhanulusal@hotmail.com>
2023-08-19 00:03:00 +0100

    implemented form validation with custom error messages

---
+++ js/formValidation.js
@@ -0,0 +1,17 @@
+// variables to hold input fields
+const name = document.getElementById("name");
+const number = document.getElementById("number");
+const email = document.getElementById("email");
+
+// setting custom error messages for the inputs
+name.oninvalid = function(event) {
+    event.target.setCustomValidity("• Cannot be empty\n• Should contain only upper and lowercase letters\n• Maximum 15 characters");
+}
+
+number.oninvalid = function(event) {
+    event.target.setCustomValidity("• Cannot be empty\n• Should contain only digits\n• Number should be 10-11 digits");
+}
+
+email.oninvalid = function(event) {
+    event.target.setCustomValidity("• Cannot be empty \n• Should be a valid email address e.g. example@example.com");
+}
